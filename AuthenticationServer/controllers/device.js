63a0047be32757e28e144fa3727af3d6f9d268bb
Mohammad Khatibzadeh <mohammad.khatibzadeh.3@gmail.com>
2022-11-28 20:38:54 +0330

    promise chain turned into async await

--- AuthenticationServer/controllers/device.js
+++ AuthenticationServer/controllers/device.js
@@ -3,45 +3,47 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 // Create new Device in Database(SQL)
-exports.postAddDevice = (req, res, next) => {
+exports.postAddDevice = async (req, res, next) => {
   const location = req.body.location;
   const sensorType = req.body.sensortype;
   console.log(location);
   console.log(sensorType);
-  Device.create({ location: location, sensortype: sensorType })
-    .then((result) => {
-      console.log(result);
-      res.status(200).json({
-        success: true,
-        data: {
-          message: "Device has been Added",
-          id: result.dataValues.id,
-        },
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+
+  const result = await Device.create({
+    location: location,
+    sensortype: sensorType,
+  }).catch((err) => {
+    console.log(err);
+    return err;
+  });
+
+  console.log(result);
+  res.status(200).json({
+    success: true,
+    data: {
+      message: "Device has been Added",
+      id: result.dataValues.id,
+    },
+  });
 };
 
 // Return all registered ids for devices(IOT device server to fake generate data with real ids)
-exports.getDeviceIds = (req, res, next) => {
-  Device.findAll()
-    .then((devices) => {
-      console.log(devices);
-      // extract the ids from devices object
-      const arrayOfIds = devices.map((device) => {
-        return device.dataValues.id;
-      });
-      res.status(200).json({
-        success: true,
-        data: {
-          message: "Device has been fetched",
-          id: arrayOfIds,
-        },
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+exports.getDeviceIds = async (req, res, next) => {
+  const devices = await Device.findAll().catch((err) => {
+    console.log(err);
+    return err;
+  });
+
+  console.log(devices);
+  // extract the ids from devices object
+  const arrayOfIds = devices.map((device) => {
+    return device.dataValues.id;
+  });
+  res.status(200).json({
+    success: true,
+    data: {
+      message: "Device has been fetched",
+      id: arrayOfIds,
+    },
+  });
 };
